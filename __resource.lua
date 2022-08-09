b59a9284fb01b6f2b1390cd96ccf33a983ef32ba
E2Ghibli <41478980+E2Ghibli@users.noreply.github.com>
2022-08-09 17:22:21 +0200

    v1.01

--- __resource.lua
+++ __resource.lua
@@ -1,18 +1,17 @@
-description 'Rojo Loading Screen'
+description 'g-loadscreen by E2 Ghibli'
 
 files {
     'index.html',
 
     'css/style.css',
-    'css/icomoon.css',
     
     'fonts/icomoon.ttf',
     
+    'js/color-change.js',
     'js/loadingbar.js',
     'js/music-controls.js',
     'js/music-handler.js',
-    'js/rojo.js',
-    'js/script.js',
+    'js/server-name-changer.js',
     'js/scriptnav.js',
     'js/config.js',
 }
