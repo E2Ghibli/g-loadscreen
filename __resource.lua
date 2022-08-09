f00308f03cf32121701d5a468e0747dbdada11f7
E2Ghibli <41478980+E2Ghibli@users.noreply.github.com>
2022-08-09 17:18:31 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,20 @@
+description 'Rojo Loading Screen'
+
+files {
+    'index.html',
+
+    'css/style.css',
+    'css/icomoon.css',
+    
+    'fonts/icomoon.ttf',
+    
+    'js/loadingbar.js',
+    'js/music-controls.js',
+    'js/music-handler.js',
+    'js/rojo.js',
+    'js/script.js',
+    'js/scriptnav.js',
+    'js/config.js',
+}
+
+loadscreen 'index.html'
